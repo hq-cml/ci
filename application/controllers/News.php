ac7661566a7c54d71f77b2b6bce9faf26d06c669
hq <hq_cml@163.com>
2016-03-02 07:33:40 +0800

    news system

---
+++ application/controllers/News.php
@@ -0,0 +1,14 @@
+<?php
+class News extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('news_model');
+        $this->load->helper('url_helper');
+    }
+
+    public function index()
+    {
+        
+    }
+}
