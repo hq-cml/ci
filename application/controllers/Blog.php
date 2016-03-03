75efdb01c8d13d0c204cc5424d99cd16542b4d55
hq <hq_cml@163.com>
2016-03-03 08:31:18 +0800

    blog

---
+++ application/controllers/Blog.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: root
+ * Date: 3/3/16
+ * Time: 8:27 AM
+ */
+class Blog extends CI_Controller {
+
+    public function index()
+    {
+        echo 'Hello World!';
+    }
+
+}
