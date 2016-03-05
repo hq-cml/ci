7a646ef657106d8691812fe37c0d5ba140fe6f12
hq <hq_cml@163.com>
2016-03-05 11:39:03 +0800

    ci drivers

---
+++ application/libraries/Connector/Connector.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Connector extends CI_Driver_Library
+{
+    public $valid_drivers;
+    public $CI;
+
+    function __construct()
+    {
+        $this->CI = &get_instance();
+        $this->valid_drivers = array('connector_twitter', 'connector_facebook');
+    }
+
+    public function get_friends()
+    {
+        return $this->twitter->get_friends() . $this->facebook->get_friends();
+    }
+}
