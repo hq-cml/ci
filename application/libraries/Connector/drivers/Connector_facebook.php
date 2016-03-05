dde147fa9934c66cf371a54f9541cb92602e3179
hq <hq_cml@163.com>
2016-03-05 11:43:52 +0800

    ci drivers:connector2

---
+++ application/libraries/Connector/drivers/Connector_facebook.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Connector_facebook extends CI_Driver
+{
+    public function get_friends()
+    {
+        return 'Ivan,  Stepan ';
+    }
+}
